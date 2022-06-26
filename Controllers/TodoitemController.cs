9c7b3e893cf88142cbc799050cd1365b72450b0b
AliElnomairi <alielnomairi@yahoo.com>
2022-06-26 19:57:17 +0300

    Initial commit

---
+++ Controllers/TodoitemController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using firstwebapi.Models;
+using System;
+namespace firstwebapi.Controllers
+{
+    [Route("api/Todoitem")]
+    [ApiController]
+    public class TodoitemController : ControllerBase
+    {
+        List <Todoitem> items= new List<Todoitem> ();
+        public TodoitemController()
+        {
+            items.Add =new Todoitem { Id = 1, Title = "item1", complete = false};
+            items.Add = new Todoitem { Id = 2, Title = "item2", complete = true };
+        }
+        [HttpGet]
+        public List<Todoitem> Get()
+        {
+            return items;
+        }
+    }
+}
---
+++ Models/Todoitem.cs
@@ -0,0 +1,9 @@
+﻿namespace firstwebapi.Models
+{
+    public class Todoitem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool complete { get; set; }
+    }
+}
